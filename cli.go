b34e5cf9acbb288e1e267ff57eb3b96da794a1c7
Nelson Neves <nelson.s.neves@gmail.com>
2016-06-20 15:10:12 +0100

    use global var debug to enable kafka debug

--- cli.go
+++ cli.go
@@ -9,7 +9,7 @@ func configureCli() (app *cli.App) {
 	app.Usage = "Supervisor"
 	app.Version = "0.0.1"
 	app.Action = func(c *cli.Context) {
-		run(c.String("listen"), c.String("kafka-brokers"))
+		run(c.String("listen"), c.String("kafka-brokers"), c.GlobalBool("debug"))
 	}
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -21,6 +21,11 @@ func configureCli() (app *cli.App) {
 			Name:  "kafka-brokers, kb",
 			Usage: "Comma separated list of kafka brokers",
 			Value: "127.0.0.1:9092",
+		},
+		cli.BoolFlag{
+			Name:   "debug",
+			Usage:  "Enable debug mode & verbose logging",
+			EnvVar: "BABL_DEBUG",
 		},
 	}
 	return
