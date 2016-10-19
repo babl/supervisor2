994b8891aef0f60a7db25e65ddb159036b097ebe
Lars Kluge <l@larskluge.com>
2016-10-19 12:24:44 +0200

    helper: convert topic to module name

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -19,4 +20,17 @@ func Hostname() string {
 
 func RequestPathToTopic(method string) string {
 	return strings.Replace(method[1:], "/", ".", 1)
+}
+
+func TopicToModuleName(topic string) string {
+	parts := strings.Split(topic, ".")
+	owner := parts[1]
+	camel := parts[2]
+
+	name := regexp.MustCompile(`[A-Z]+`).ReplaceAllStringFunc(camel, func(m string) string {
+		return "-" + strings.ToLower(m)
+	})
+	name = strings.TrimLeft(name, "-")
+
+	return owner + "/" + name
 }
--- utils_test.go
+++ utils_test.go
@@ -11,4 +11,15 @@ var _ = Describe("Utils", func() {
 			Expect(RequestPathToTopic("/babl.larskluge.Telegram/IO")).To(Equal("babl.larskluge.Telegram.IO"))
 		})
 	})
+	Context("#TopicToModuleName", func() {
+		It("converts a Kafka topic to module name", func() {
+			Expect(TopicToModuleName("babl.larskluge.Telegram.IO")).To(Equal("larskluge/telegram"))
+		})
+		It("supports two word module names", func() {
+			Expect(TopicToModuleName("babl.larskluge.RenderWebsite.IO")).To(Equal("larskluge/render-website"))
+		})
+		It("also works with a meta topic", func() {
+			Expect(TopicToModuleName("babl.babl.Events.Meta")).To(Equal("babl/events"))
+		})
+	})
 })
